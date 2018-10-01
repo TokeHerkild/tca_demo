5bf888a56bdc6d7654f84ada97d7d33e2e351df0
Toke Herkild <toke@webto.dk>
2018-10-01 13:10:32 +0200

    corrected string replacements

--- install.sh
+++ install.sh
@@ -3,17 +3,21 @@
 echo Enter extKey:
 read EXTKEY
 
+extKey=${EXTKEY/\_/\-}
+echo $extKey
+
 echo Enter ExtName:
 read EXTNAME
 
 echo Enter description:
 read DESCRIPTION
 
-sed -i 's/{EXTKEY}/'$EXTKEY'/' composer.json
+sed -i "s/{extKey}/$extKey/" composer.json
+sed -i "s/{EXTKEY}/$EXTKEY/" composer.json
 
-sed -i 's/{EXTNAME}/'$EXTNAME'/' composer.json
+sed -i "s/{EXTNAME}/$EXTNAME/" composer.json
 
-sed -i 's/{DESCRIPTION}/'$DESCRIPTION'/' composer.json
+sed -i "s/{DESCRIPTION}/$DESCRIPTION/" composer.json
 
 echo composer.json updated.
 echo
